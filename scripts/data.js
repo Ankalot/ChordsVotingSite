a18b823c3eed66d7408578d67e93184d1f6ef84e
Ankalot <kotov_andrei-62@mail.ru>
2023-12-21 20:09:31 +0300

    fix data tab

--- scripts/data.js
+++ scripts/data.js
@@ -1,9 +1,12 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+
 const supabase = createClient("https://bdyvbrbsjpueshknxyjk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkeXZicmJzanB1ZXNoa254eWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwNjUwMTUsImV4cCI6MjAxODY0MTAxNX0.YQpF30vC98GeAzll0Sz6YXg5gTSaB9tHJn23OR-2PG8");
 // Don't hack my db pls :C
 
 const {data, error} = await supabase.from('ChordsComparison').select(); 
 if (error) {
     alert(error);
-    return;
+} else {
+    document.getElementById("dataHandler").textContent = JSON.stringify(data)
 }
-document.getElementById("dataHandler").textContent = JSON.stringify(data)
